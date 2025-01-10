8adbfb09e84ec11d1cfe1ce79cd30b854213ad75
gimtwinkle <gimtwinkle@gmail.com>
2025-01-10 11:07:45 +0900

    포스팅 상세 해당 게시물만 나오도록 수정

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -2,40 +2,42 @@ import React, { useEffect, useState } from 'react'
 import ListTitle from '../components/ListTitle'
 import ListContent from '../components/ListContent'
 import Button from '../components/Button'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { CreatePost } from './CreatePost'
-import { collection, getDocs } from 'firebase/firestore'
+import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 import { UpdatePost } from "./UpdatePost";
 
 export function PostDetail() {
-    const [getLists, setGetLists] = useState([])
-
+    const params = useParams();
+    const [getPost, setGetPost] = useState('');
+    
     useEffect(() => {
-        const fetchLists = async () => {
+        const fetchPost = async () => {
+            const docRef = doc(db, "blogg", `${params.listId}`);
+            console.log(params);
+            const docSnap = await getDoc(docRef);
             try {
-                const querySnapshot = await getDocs(collection(db, "blogg"));
-                const listsData = [];
-                querySnapshot.forEach((doc) => {
-                    listsData.push(doc.id,doc.data());
-                });
-                setGetLists(listsData);
-                console.log()
+                if (docSnap.exists()) {
+                  setGetPost(docSnap.data());
+                } else {
+                  // docSnap.data() will be undefined in this case
+                  console.log("No such document!");
+                }
+                
             } catch(error) {
                 console.log(error);
             }
         }
-        fetchLists();
-    }, [])
+        fetchPost();
+    }, [params])
     
     return (
         <>
-            {getLists.map(item => (
-                <div key={item.id}>
-                    <ListTitle>{item.title}</ListTitle>
-                    <ListContent>{item.content}</ListContent>
-                </div>
-            ))}
+            <div>
+                <ListTitle>{getPost.title}</ListTitle>
+                <ListContent>{getPost.content}</ListContent>
+            </div>
             <Link to='/CreatePost' element={<CreatePost />}>
                 <Button>작성하기</Button>
             </Link>
